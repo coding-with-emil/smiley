501289eefe8bd77a815f5df2ddd4e737e5b6d150
Coding with Emil <codingwithemilyt@gmail.com>
2024-09-15 08:53:40 +0200

    Added main.c

--- main.c
+++ main.c
@@ -2,6 +2,6 @@
 
 int main(int argc, char** argv)
 {
-	MessageBoxA(0, "I am so happy", "smiley.exe", MB_ICONINFORMATION | MB_OK);
+	MessageBoxA(0, "I am so happy to be part of section 1", "smiley.exe", MB_ICONINFORMATION | MB_OK);
 	return 0;
 }
